4bba9818ab81d670d9d0d14697d91d485038339b
jorgealcalah <jorgealcala1986@gmail.com>
2021-04-14 11:31:11 -0500

    errors in include files fixed

--- include/UAVIncludes.h
+++ include/UAVIncludes.h
@@ -1,6 +1,12 @@
 #include <Arduino.h>
 #include "UAV.h"
 #include "Communications.h"
+#include <BluetoothSerial.h>
+#include <ImuBNO080.h>
+#include <BaroBMP388.h>
 
 UAV uav;
 Communications communication;
+BluetoothSerial SerialBT;
+IMU imu_080;
+Baro baro;
