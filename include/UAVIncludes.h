a3a789ef410d077a5fcf901bea9beb80e6aa550a
jorgealcalah <jorgealcala1986@gmail.com>
2021-04-14 22:01:24 -0500

    cool includes

--- include/UAVIncludes.h
+++ include/UAVIncludes.h
@@ -2,18 +2,19 @@
 #include "UAV.h"
 #include "Communications.h"
 #include <BluetoothSerial.h>
-#include <ImuBNO080.h>
-#include <BaroBMP388.h>
+
+
+//#define ADDR_PIN 17
 
 UAV uav;
 Communications communication;
-BluetoothSerial SerialBT;
-IMU imu_080;
-Baro baro;
+//BluetoothSerial SerialBT;
+//IMU imu_080;
+//Baro baro;
 
 char imu_string[48];
 char baro_string[16];
-const byte LED_PIN = 15;
+//const byte LED_PIN = 15;
 uint16_t dt = 20;
 uint16_t blink_period = 500;
 ulong previous_millis, current_millis;
--- lib/Communications/Communications.cpp
+++ lib/Communications/Communications.cpp
@@ -1,5 +1,12 @@
 #include "Communications.h"
 #include "MessageUtilities.h"
+#include <ImuBNO080.h>
+#include <BaroBMP388.h>
+#include <BluetoothSerial.h>
+
+#define ADDR_PIN 17
+const byte LED_PIN = 15;
+BluetoothSerial SerialBT;
 Communications::Communications()
 {
 }
@@ -11,6 +18,11 @@ Communications::~Communications()
 void Communications::begin()
 {
     Serial.begin(115200);
+    SerialBT.begin("Nimu");
+    pinMode(ADDR_PIN, OUTPUT);
+    digitalWrite(ADDR_PIN, HIGH);
+    pinMode(LED_PIN, OUTPUT);
+    digitalWrite(LED_PIN, LOW);
 }
 
 void Communications::sendData(double data[], uint8_t decimals)
