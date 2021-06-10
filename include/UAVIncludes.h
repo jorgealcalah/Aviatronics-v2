b6a221e9434d7948e4c8f48daaa4d228c289ec05
jorgealcalah <jorgealcala1986@gmail.com>
2021-06-09 20:40:53 -0500

    actualizado uavincludes

--- include/UAVIncludes.h
+++ include/UAVIncludes.h
@@ -2,16 +2,26 @@
 #include "UAV.h"
 #include "Communications.h"
 #include <BluetoothSerial.h>
-
+#include <PID_controller.h>
+#include <RefRead.h>
+#include <ESP32_PWM.h>
 //Interrupt Variables
 int totalInterruptCounter;
 volatile bool interruptFlag = LOW;
 hw_timer_t *timer = NULL;
 portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
 
-//Object definitions
-UAV uav;
+//Control objects
+PID_controller PID;
+PID_controller PID2;
+RefRead Reference;
+PWM pwm1;
+PWM pwm2;
+
+//class definitions
+UAV uav; //not used
 Communications communication;
+BluetoothSerial BT;
 
 //polling variables (not used)
 uint16_t dt_n = 100;
