499f8d0866264e6d36c6c840da7d25a258e7bc3a
jorgealcalah <jorgealcala1986@gmail.com>
2021-06-10 12:40:52 -0500

    comentación del main xD

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <UAVincludes.h>
 
-void IRAM_ATTR onTimer()
+void IRAM_ATTR onTimer() //rutina de atención de timer interrupt
 {
   portENTER_CRITICAL_ISR(&timerMux);
   interruptFlag = HIGH;
@@ -8,30 +8,31 @@ void IRAM_ATTR onTimer()
 }
 void setup()
 {
-  pwm1.setup(PWMPitchPin, 1, 250);
+
+  pwm1.setup(PWMPitchPin, 1, 250); //configuración de señales PWM para controlar pitch y roll
   pwm2.setup(PWMRollPin, 2, 250);
-  communication.begin();
-  timer = timerBegin(0, 80, true); //se usa el timer 0 (de 4 disponibles), prescaler de 80 MHz, true para que cuente hacia arriba
-  timerAttachInterrupt(timer, &onTimer, true);
-  timerAlarmWrite(timer, 1000, true);
-  timerAlarmEnable(timer);
+  communication.begin();                       //incializa baud rate y conexión BT con la ESP32
+  timer = timerBegin(0, 80, true);             //se usa el timer 0 (de 4 disponibles), prescaler de 80 MHz, true para que cuente hacia arriba
+  timerAttachInterrupt(timer, &onTimer, true); //se inicializa interrupt
+  timerAlarmWrite(timer, 1000, true);          // se establecen 1000 microsegundos (1 milisegundo) para el timer. Más info en https://techtutorialsx.com/2017/10/07/esp32-arduino-timer-interrupts/#:~:text=The%20prescaler%20is%20used%20to,a%20lot%20of%20configuration%20freedom.
+  timerAlarmEnable(timer);                     //se habilita el timer
 
   while (!Wire.begin())
   {
     Serial.print("Sensors failed");
     delay(500);
   }
-  communication.initializeSensors();
+  communication.initializeSensors(); //inicializa IMU, barómetro, GPS y el bus I2C
 }
 
 void loop()
 {
-  if (interruptFlag == HIGH)
+  if (interruptFlag == HIGH) //rutina de interrupción de timer
   {
     portENTER_CRITICAL(&timerMux);
     interruptFlag = LOW;
     portEXIT_CRITICAL(&timerMux);
-    communication.sendData(pitchRef, rollRef);
+    communication.sendData(pitchRef, rollRef); //manda las referencias de pitch y roll y las imprime con los otros datos
   }
   communication.checkDataIMU();
   communication.displayGPSinfo();
