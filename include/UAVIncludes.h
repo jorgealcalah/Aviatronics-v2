32e047e2f4e45cf1d58236d391c9d73c647f331c
jorgealcalah <jorgealcala1986@gmail.com>
2021-06-09 21:04:49 -0500

    unnecessary lines deleted and control variables moved to uavincludes

--- include/UAVIncludes.h
+++ include/UAVIncludes.h
@@ -6,6 +6,24 @@
 #include <RefRead.h>
 #include <ESP32_PWM.h>
 
+//PID variables
+#define pitch_Kp 0.6
+#define Pitch_Ki 0.4
+#define pitch_Kd 0.0
+#define roll_Kp 0.6
+#define roll_ki 0.3
+#define roll_kd 0.0
+#define sampleTime 0.038
+#define PWMPitchPin 27
+#define PWMRollPin 26
+float pitchOut;
+float rollOut;
+float pitchRef;
+float rollRef;
+float pitchU;
+float rollU;
+//PID_controller pitchValue;
+
 //Interrupt Variables
 int totalInterruptCounter;
 volatile bool interruptFlag = LOW;
--- lib/Communications/Communications.cpp
+++ lib/Communications/Communications.cpp
@@ -40,14 +40,14 @@ void Communications::initializeSensors()
     }
     Serial.println("GPS module found!");
 }
-void Communications::sendData(float _pitchRef,float _rollRef)
+void Communications::sendData(float _pitchRef, float _rollRef)
 {
     baro.readSensors(baro_measurements);
     buildString(baro_measurements, sizeof(baro_measurements) / sizeof(baro_measurements[0]), baro_string, "%.2f,");
     buildString(imu_measurements, sizeof(imu_measurements) / sizeof(imu_measurements[0]), imu_string, "%.2f, ");
     buildString(gps_measurements, sizeof(gps_measurements) / sizeof(gps_measurements[0]), gps_string, "%.6f, ");
     Serial.printf("%lu, %s%s%s\n", millis(), imu_string, baro_string, gps_string);
-    SerialBT.printf("%lu, %s%s%s%.2f%s%.2f\n", millis(), imu_string, baro_string, gps_string,_pitchRef,",",_rollRef);
+    SerialBT.printf("%lu, %s%s%s%.2f%s%.2f\n", millis(), imu_string, baro_string, gps_string, _pitchRef, ",", _rollRef);
 }
 float Communications::checkDataIMU()
 {
@@ -58,21 +58,23 @@ float Communications::checkDataIMU()
     }
 }
 
-float Communications::checkRoll(){
+float Communications::checkRoll()
+{
     return imu_measurements[0];
 }
 
-
-float Communications::checkPitch(){
+float Communications::checkPitch()
+{
     return imu_measurements[1];
 }
 
-float Communications::checkYaw(){
+float Communications::checkYaw()
+{
     return imu_measurements[2];
 }
 void Communications::displayGPSinfo()
 {
-    
+
     while (myI2CGPS.available()) //available() returns the number of new bytes available from the GPS module
     {
         gps.encode(myI2CGPS.read()); //Feed the GPS parser
